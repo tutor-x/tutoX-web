9ce4f251c8e44faafed3b84e40eab0fb936beee1
Sabith <mohamedsabith2@gmail.com>
2023-09-24 00:28:01 +0530

    initial setup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import ProfilePage from "./pages/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+// import { themeSettings } from "./theme";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mode = useSelector((state) => state.mode);
+  // const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // <div className="app">
+    //   <BrowserRouter>
+    //     {/* <ThemeProvider theme={theme}> */}
+    //       {/* <CssBaseline /> */}
+    //       <Routes>
+    //         <Route path="/" element={<LoginPage />} />
+    //         <Route
+    //           path="/home"
+    //           element={isAuth ? <HomePage /> : <Navigate to="/" />}
+    //         />
+    //         <Route
+    //           path="/profile/:userId"
+    //           element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+    //         />
+    //       </Routes>
+    //     {/* </ThemeProvider> */}
+    //   </BrowserRouter>
+    // </div>
+
+    /*
+      Here I have disabled authentication check for development purpose only
+      Above given code has the auth check after the ui works done we can remove below code
+      use the above code
+    */
+    <div className="app">
+      <BrowserRouter>
+        {/* <ThemeProvider theme={theme}> */}
+          {/* <CssBaseline /> */}
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={<HomePage />}
+            />
+            <Route
+              path="/profile"
+              element={<ProfilePage />}
+            />
+          </Routes>
+        {/* </ThemeProvider> */}
+      </BrowserRouter>
+    </div>
   )
 }
 
